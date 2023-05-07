39be1b3daeb01cc4b1609bf147b1fbc92e23758b
Kunal Mandalya <kunalmandalya@Kunals-MacBook-Air.local>
2023-05-07 13:31:51 -0400

    resolve conflicts with latest

--- client/src/components/companyDetails/Company.js
+++ client/src/components/companyDetails/Company.js
@@ -1,39 +1,36 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState, useEffect } from "react";
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import TextField from '@mui/material/TextField';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import CircularProgress from '@mui/material/CircularProgress';
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -62,28 +59,36 @@ function TablePaginationActions(props) {
         disabled={page === 0}
         aria-label="first page"
       >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
       <IconButton
         onClick={handleBackButtonClick}
         disabled={page === 0}
         aria-label="previous page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="last page"
       >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
     </Box>
   );
@@ -96,22 +101,23 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-
 export default function CustomPaginationActionsTable() {
+  const [originaldata, setData] = useState([]);
   const [rows, setFilteredRows] = useState([]);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(false);
- 
+
   useEffect(() => {
     async function fetchData() {
-     const { data } = await axios.get(`http://localhost:8000/`);
+      const { data } = await axios.get(`http://localhost:8000/`);
       //console.log(data)
+      setData(data.data);
       setFilteredRows(data.data);
-   }
-   fetchData();
- }, []);
+    }
+    fetchData();
+  }, []);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -119,13 +125,14 @@ export default function CustomPaginationActionsTable() {
   };
 
   const handleSearch = (searchTerm) => {
-    const filteredData = rows.filter((row) =>
+    setFilteredRows(originaldata);
+    const filteredData = originaldata.filter((row) =>
       row.employer_name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredRows(filteredData);
     setPage(0);
   };
-  
+
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -140,76 +147,79 @@ export default function CustomPaginationActionsTable() {
   };
 
   return (
-        <div>
-             <TextField
-         className='search-bar'
-      label="Search Company"
-      value={searchTerm}
-      onChange={handleChange}
-    />
-       
-    <div className='TableCompany'>
-        
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-        
-      <TableHead>
-          <TableRow>
-            <StyledTableCell>Company Name</StyledTableCell>
-            <StyledTableCell align="right">Approved Applications</StyledTableCell>
-            <StyledTableCell></StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <TableRow key={row.employer_name}>
-              <TableCell component="th" scope="row">
-              <Link to={`/employee/${row._id}`}> {row.employer_name}</Link>
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.certified_count}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.fat}
-              </TableCell>
-            </TableRow>
-          ))}
+    <div>
+      <TextField
+        className="search-bar"
+        label="Search Company"
+        value={searchTerm}
+        onChange={handleChange}
+      />
 
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+      <div className="TableCompany">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Company Name</StyledTableCell>
+                <StyledTableCell align="right">
+                  Approved Applications
+                </StyledTableCell>
+                <StyledTableCell></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? rows.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : rows
+              ).map((row) => (
+                <TableRow key={row.employer_name}>
+                  <TableCell component="th" scope="row">
+                    <Link to={`/employee/${row._id}`}>
+                      {" "}
+                      {row.employer_name}
+                    </Link>
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {row.certified_count}
+                  </TableCell>
+                  <TableCell style={{ width: 160 }} align="right">
+                    {row.fat}
+                  </TableCell>
+                </TableRow>
+              ))}
+
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[10, 15]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      "aria-label": "rows per page",
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
-    </div>
-    
   );
- 
 }
-
