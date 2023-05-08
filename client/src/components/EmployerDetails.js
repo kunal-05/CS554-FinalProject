58d7417a2f9420a6d31c694e0a48813063133dc7
parumahajan24 <parumahajan24@gmail.com>
2023-05-08 17:05:12 -0400

    resolved pull issues - rebase with kunal branch into parul

--- client/src/components/EmployerDetails.js
+++ client/src/components/EmployerDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import {
   makeStyles,
   Card,
@@ -8,9 +8,10 @@ import {
   CardMedia,
   Typography,
   CardHeader,
-  Avatar
+  Avatar,
 } from "@material-ui/core";
 import "../App.css";
+//import Index from "./Graphs/index";
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -43,7 +44,7 @@ const useStyles = makeStyles({
   titleHead: {
     borderBottom: "1px solid #1e8678",
     fontWeight: "bold",
-    textAlign: 'center'
+    textAlign: "center",
   },
   grid: {
     flexGrow: 1,
@@ -60,69 +61,161 @@ const useStyles = makeStyles({
   },
 });
 
-const EmployerDetails =  (props) => {
-  const [showData, setShowData] = useState([]);
+const EmployerDetails = (props) => {
+  const [showData, setShowData] = useState("");
   const [loading, setLoading] = useState(true);
   // const [data, setData] = useState([]);
   const classes = useStyles();
 
   const { id } = useParams();
-useEffect(()=>{
-  async function fetchData(){
-    const {data} = await axios.get(`http://localhost:8000/${id}`);
-    //console.log(data.data)
-    setShowData(data.data)
+  //id validation
+  if (!id) {
+    throw new Error(`NO ID was provided`);
   }
-  fetchData("showData-->",showData);
-},[])
+  if (typeof id !== "string" || id == null) {
+    throw new Error(`ID must be a string`);
+  }
+  if (id.trim().length === 0 || id === "") {
+    throw new Error(`Invalid ID`);
+  }
 
-  // if (loading) {
-  //   return (
-  //     <div>
-  //       <h2>Loading....</h2>
-  //     </div>
-  //   );
-  // } else if(showData) {
-  return (
-    <div className={classes.root}>
-      <Card className={classes.card} variant="outlined">
-        <CardHeader
-          className={classes.titleHead}
-          title={showData.employer_name}
-        />
-        <CardContent>
-          <Typography variant="subtitle1" color="textPrimary" className={classes.dt} component="span">
-            NAME:{showData.employer_name}
-          </Typography>
-          <br/>
-          <Typography variant="h5" className={classes.dt} color="textPrimary" component="span">
-            ADDRESS: {showData.employer_city + "," + showData.employer_state}
-          </Typography>
-          <br/>
-          <Typography variant="h5" className={classes.dt} color="textPrimary" component="span">
-            ADDRESS: {showData.employer_city + "," + showData.employer_state}
-          </Typography>
-        </CardContent>
-      </Card>
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await axios.get(`http://localhost:8000/${id}`);
+        console.log("data recieved from backend-->", data.data);
+        if (!data.data) {
+          setLoading(true);
+        } else if (data.data === "NoData") {
+          setLoading(false);
+          throw new Error(`ERROR 404 - No data with id: ${id}`);
+        } else {
+          setShowData(data.data);
+          setLoading(false);
+        }
+      } catch (e) {
+        console.log("error in fetching data -> ", e);
+      }
+    }
+    fetchData();
+  }, [id]);
 
-      <Card className={classes.cardSecond} variant="outlined">
-        <CardHeader
-          className={classes.titleHead}
-          title={showData.employer_name}
-        />
-        <CardContent>
-          <Typography variant="body2" color="textPrimary" component="span">
-            This will display chart information
-            <Link to="/">Back to company listing page...</Link>
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>    
-  );
-  // }
-  // else{
-  //   return <p><NoDataFound/> </p>
-  //  }
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading....</h2>
+      </div>
+    );
+  } else if (showData) {
+    console.log("showDtaa is -->", showData);
+    const jobTitles = showData.job_title ? Object.keys(showData.job_title) : [];
+    //console.log("jobtitles -->", jobTitles.length);
+    return (
+      <div className={classes.root}>
+        <Card className={classes.card} variant="outlined">
+          <CardHeader
+            className={classes.titleHead}
+            title={showData.employer_name}
+          />
+          <CardContent>
+            <Typography
+              variant="subtitle1"
+              color="textPrimary"
+              className={classes.dt}
+              component="span"
+            >
+              NAME: {showData.employer_name}
+            </Typography>
+            <br />
+            <Typography
+              variant="h5"
+              className={classes.dt}
+              color="textPrimary"
+              component="span"
+            >
+              ADDRESS:{" "}
+              {showData &&
+              showData.employer_address &&
+              showData.employer_city &&
+              showData.employer_postal_code &&
+              showData.employer_state
+                ? showData.employer_address +
+                  "," +
+                  showData.employer_city +
+                  "," +
+                  showData.employer_postal_code +
+                  "," +
+                  showData.employer_state
+                : "N/A"}
+            </Typography>
+            <br />
+            <Typography
+              variant="h5"
+              className={classes.dt}
+              color="textPrimary"
+              component="span"
+            >
+              Certified Visa Cases:{" "}
+              {showData.certified_count ? showData.certified_count : "0"}
+            </Typography>{" "}
+            <br />
+            <Typography
+              variant="h5"
+              className={classes.dt}
+              color="textPrimary"
+              component="span"
+            >
+              Denied Visa Cases:{" "}
+              {showData.denied_count ? showData.denied_count : "0"}
+            </Typography>
+            <br />
+            <Typography
+              variant="h5"
+              className={classes.dt}
+              color="textPrimary"
+              component="span"
+            >
+              {jobTitles.length ? (
+                <p>
+                  Job Titles:
+                  <ul>
+                    {jobTitles.length
+                      ? jobTitles?.map((title, index) => (
+                          <li key={index}>{title}</li>
+                        ))
+                      : "N/A"}
+                  </ul>
+                </p>
+              ) : (
+                ""
+              )}
+            </Typography>
+            <Typography variant="body2" color="textPrimary" component="span">
+              <Link to="/" style={{textAlign: 'center'}}>
+                {" "}
+                Go through company listing again?
+              </Link>
+            </Typography>
+          </CardContent>
+        </Card>
+
+        {/* <Card className={classes.cardSecond} variant="outlined">
+          <CardHeader
+            className={classes.titleHead}
+            title={"Year wise Confirmed and Rejected Visa Cases"}
+          />
+          <CardContent>
+            <p>will display charts here!</p>
+          </CardContent>
+        </Card> */}
+        {/* <div>
+          <Index />
+        </div> */}
+      </div>
+    );
+  } else {
+    return <p>NO data found for id : {id}</p>;
+  }
 };
 
 export default EmployerDetails;
--- server/routes/employers.js
+++ server/routes/employers.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const redis = require('redis');
+const redis = require("redis");
 const client = redis.createClient();
+<<<<<<< HEAD
 const importedData = require("../data/FinalEmployerDataJobTitle.json")
 const citizenData = require("../data/CitizenShip.json")
 const yearData = require("../data/YearWise.json")
+=======
+const importedData = require("../data/FinalEmployerDataJobTitle.json");
+>>>>>>> d388f6a (updated emplyerDetails page and redis)
 client.connect().then(() => {});
 
-router.get('/', async (req, res) => {
-  console.log('Show List not cached');
-  let data = importedData
-
-  console.log(data)
-  return res.json({"data":data})
+router.get("/", async (req, res) => {
+  console.log("Show List not cached -  home route /");
+  let cachedFullCompanyData = await client.get("data");
+  if (cachedFullCompanyData) {
+    let cachedCompanyList = JSON.parse(cachedFullCompanyData);
+    console.log(`--- Full data returned from redis cache ---`);
+    return res
+      .status(200)
+      .json({ data: cachedCompanyList ? cachedCompanyList : `NoData` });
+  } else {
+    //get the data and store in redis
+    let data = importedData;
+    const key = `data`;
+    let cacheData = await client.set(key, JSON.stringify(data));
+    console.log(`Full data stored in cache -->`, cacheData);
+    return res.status(200).json({ data: data });
+  }
 });
 
 router.get('/employeeCitizenship', async (req, res) => {
@@ -27,16 +42,43 @@ router.get('/yearData', async (req, res) => {
   return res.json({"data":data})
 });
 
-router.get('/:id', async (req, res) => {
-  console.log('data not cached');
-  let data = importedData
+router.get("/:id", async (req, res) => {
+  let data = importedData;
   const id = req.params.id;
+  console.log("server id ->", id);
+  //id validations
+  if (!id) {
+    return res.status(400).json({
+      error: "ERROR: No id is provided.",
+    });
+  }
+  if (typeof id !== "string" || id == null) {
+    res.status(400).json({ error: "ERROR: ID must be a string" });
+    return;
+  }
+  if (id.trim().length === 0 || id == "") {
+    return res.status(400).json({
+      error:
+        "ERROR: Invalid ID in req.params - must be a string and no empty spaces",
+    });
+  }
 
-  console.log("id in backend-->",id)
-  const showData = data.find((obj) => obj._id === id);
-  //return showData ? foundObj.name : "Not found";
-
-  console.log("SHowdata from backeend-->",showData);
-  return res.json({"data":showData? showData: 'No Data'})
+  //check if id stored in cache?
+  let cachedDataById = await client.get(id);
+  if (cachedDataById) {
+    let cachedEmployerDetails = JSON.parse(cachedDataById);
+    console.log(`Data for id: ${id} is returned from Redis`);
+    return res
+      .status(200)
+      .json({ data: cachedEmployerDetails ? cachedEmployerDetails : `NoData` });
+  } else {
+    //cache the data first then return
+    const showData = data.find((obj) => obj._id === id);
+    const key = `${id}`;
+    let cacheData = await client.set(key, JSON.stringify(showData));
+    console.log(`Data cahced for id: ${id} -->`, cacheData);
+    return res.status(200).json({ data: showData ? showData : `NoData` });
+  }
 });
+
 module.exports = router;
