0c4db6894b684aa919d65ee50401afa23551e9a1
Kunal Mandalya <kunalmandalya@Kunals-MacBook-Air.local>
2023-05-08 18:18:17 -0400

    rebase conflicts

--- client/src/components/companyDetails/Graph4.js
+++ client/src/components/companyDetails/Graph4.js
@@ -5,28 +5,10 @@ import axios from "axios";
 import React, { useState, useEffect,PureComponent } from "react";
 
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-
-
-// const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"]; // replace with your desired colors
 
 const MyChart = () => {
   const [originaldata, setData] = useState([]);
-  // const [rows, setFilteredRows] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const { data } = await axios.get(
